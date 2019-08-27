5b52382001738976c33387f0ccfc33074a93510f
roman_ushakov <roman.ushakov.89@inbox.ru>
2019-08-27 17:51:25 +0300

    test: check genDiff

--- src/index.js
+++ src/index.js
@@ -1,35 +1,23 @@
-import { has, flatten } from 'lodash';
-
+import { has, uniq } from 'lodash';
 
 export default (firstConfig, secondConfig) => {
-  const allKeys = [...Object.keys(firstConfig), ...Object.keys(secondConfig)];
+  const allKeys = uniq([...Object.keys(firstConfig), ...Object.keys(secondConfig)]);
 
-  const isMatched = (key) => (has(firstConfig, key) && has(secondConfig, key));
-  const isAdded = (key) => (!has(firstConfig, key) && has(secondConfig, key));
-  const isDeleted = (key) => (has(firstConfig, key) && !has(secondConfig, key));
-
-  const iter = (keys, result) => {
-    const [key, ...restKeys] = keys;
-
-    if (!key) {
-      return result;
+  return allKeys.reduce((acc, key) => {
+    if (!has(firstConfig, key) && has(secondConfig, key)) {
+      return acc.concat({ type: 'added', key, value: secondConfig[key] });
     }
 
-    return iter(restKeys, result);
-  };
+    if (has(firstConfig, key) && !has(secondConfig, key)) {
+      return acc.concat({ type: 'deleted', key, value: firstConfig[key] });
+    }
 
-  return iter(allKeys, []);
-//   return allKeys.reduce((acc, key) => {
-//     if (isAdded(key)) {
-//       return acc.concat({ type: 'added', key, value: secondConfig[key] });
-//     }
+    if (firstConfig[key] === secondConfig[key]) {
+      return acc.concat({ type: 'matched', key, value: firstConfig[key] });
+    }
 
-//     if (isDeleted(key)) {
-//       return acc.concat({ type: 'deleted', key, value: firstConfig[key] });
-//     }
-
-
-
-//     return acc.concat({ type: 'deleted', key, value: firstConfig });
-//   }, []);
+    return acc
+      .concat({ type: 'deleted', key, value: firstConfig[key] })
+      .concat({ type: 'added', key, value: secondConfig[key] });
+  }, []);
 };
