2790c39273bccf88a5fbdc9beb36e2351e191b61
roman_ushakov <roman.ushakov.89@inbox.ru>
2019-08-30 08:31:02 +0300

    test: refactoring

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -4,18 +4,12 @@ import genDiff from '../src';
 
 const expected = fs.readFileSync(path.resolve(__dirname, '__fixtures__/result.txt'), 'utf8');
 
-test('json getDataDiff', () => {
-  const beforePath = path.resolve(__dirname, '__fixtures__/before.json');
-  const afterPath = path.resolve(__dirname, '__fixtures__/after.json');
+const dataSet = [
+  [path.resolve(__dirname, '__fixtures__/before.json'), path.resolve(__dirname, '__fixtures__/after.json')],
+  [path.resolve(__dirname, '__fixtures__/before.yml'), path.resolve(__dirname, '__fixtures__/after.yml')],
+];
 
-  const result = genDiff(beforePath, afterPath);
-  expect(result).toMatch(expected);
-});
-
-test('yaml getDataDiff', () => {
-  const beforePath = path.resolve(__dirname, '__fixtures__/before.yml');
-  const afterPath = path.resolve(__dirname, '__fixtures__/after.yml');
-
+test.each(dataSet)('getDataDiff(%s, %s)', (beforePath, afterPath) => {
   const result = genDiff(beforePath, afterPath);
   expect(result).toMatch(expected);
 });
